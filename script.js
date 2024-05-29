b078ebe11829ad6774f91371b9b68f292d76ef6d
richardgoncalve <133219524+richardgoncalve@users.noreply.github.com>
2024-05-29 23:47:11 +0000

    29/05/2024

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-principal');
+const caixaAlternativas = document.querySelector('.caixa-principal');
+const caixaResultado = document.querySelector('.caixa-principal');
+const TextoResultado = document.querySelector('.caixa-principal');
+
+const perguntas =[
+  {
+    Enunciado: "Pergunta 1",
+    Alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enunciado: "Pergunta 2",
+    Alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enunciado: "Pergunta 3",
+    Alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enunciado: "Pergunta 4",
+    Alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  },
+
+  {
+    Enunciado: "Pergunta 5",
+    Alternativas: [
+      "Resposta 1",
+      "Resposta 2",
+    ]
+  }
+];
